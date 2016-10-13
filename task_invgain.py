e58967e0d47f9f5be477e4340619ed6b73c821de
Chris Hales <chris.hales.github@gmail.com>
2016-10-13 17:28:51 -0600

    code

--- task_invgain.py
+++ task_invgain.py
@@ -1,33 +1,12 @@
 from taskinit import *
 
-# Copyright (c) 2015, Christopher A. Hales
-# All rights reserved.
-#
-# Redistribution and use in source and binary forms, with or without
-# modification, are permitted provided that the following conditions are met:
-#     * Redistributions of source code must retain the above copyright
-#       notice, this list of conditions and the following disclaimer.
-#     * Redistributions in binary form must reproduce the above copyright
-#       notice, this list of conditions and the following disclaimer in the
-#       documentation and/or other materials provided with the distribution.
-#     * The NAMES OF ITS CONTRIBUTORS may not be used to endorse or promote
-#       products derived from this software without specific prior written
-#       permission.
-# 
-# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
-# ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
-# WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
-# DISCLAIMED. IN NO EVENT SHALL C. A. HALES BE LIABLE FOR ANY DIRECT, INDIRECT,
-# INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
-# LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA,
-# OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
-# LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
-# NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,
-# EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+# invgain is released under a BSD 3-Clause License
+# See LICENSE for details
 
 # HISTORY:
 #   1.0  16Sep2015  Initial version.
 #   1.1  27Sep2016  Fixed typos, no functionality change
+#   1.2  13Oct2016  Fixed license, no changes to code
 #
 
 def invgain(caltable,field,spw,scan,obsid):
@@ -38,8 +17,8 @@ def invgain(caltable,field,spw,scan,obsid):
     #    Invert gain solutions, overwriting caltable.
     #    Christopher A. Hales
     #
-    #    Version 1.1 (tested with CASA Version 4.6.0)
-    #    27 September 2016
+    #    Version 1.2 (tested with CASA Version 4.7.0)
+    #    13 October 2016
     
     casalog.origin('invgain')
     
